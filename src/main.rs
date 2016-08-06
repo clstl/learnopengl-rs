e13bc066baad63d4f8fbf76cb8cd4ab392fe7826
clstl <reamles@gmail.com>
2016-08-06 23:38:55 +0300

    Hello Triangle (Rect) with indices

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,48 @@
 #[macro_use]
 extern crate glium;
 
-use glium::Surface;
 use glium::glutin::{Api};
 use glium::glutin::Event::{Closed, KeyboardInput};
 use glium::glutin::VirtualKeyCode;
 use glium::glutin::GlRequest;
 
 
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 3],
+}
+
+implement_vertex!(Vertex, position);
+
 fn main() {
     use glium::{DisplayBuild, Surface};
+
+    //Program
+    let vertex_shader_src = r#"
+    #version 330 core
+
+    layout (location = 0) in vec3 position;
+
+    void main()
+    {
+        gl_Position = vec4(position.x, position.y, position.z, 1.0);
+    }
+    "#;
+
+    let fragment_shader_src = r#"
+    #version 330 core
+
+    out vec4 color;
+
+    void main()
+    {
+        color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    }
+
+    "#;
+
+
+    //Window
     let display = glium::glutin::WindowBuilder::new()
         .with_dimensions(800, 600)
         .with_title("learnopengl")
@@ -18,9 +51,33 @@ fn main() {
         .build_glium()
         .unwrap();
 
+    //Triangle
+    /*
+    0.5f,  0.5f, 0.0f,  // Top Right
+     0.5f, -0.5f, 0.0f,  // Bottom Right
+    -0.5f, -0.5f, 0.0f,  // Bottom Left
+    -0.5f,  0.5f, 0.0f   // Top Left
+    */
+
+    let vertex1 = Vertex { position: [0.5, 0.5, 0.0] };
+    let vertex2 = Vertex { position: [ 0.5,  -0.5, 0.0] };
+    let vertex3 = Vertex { position: [ -0.5, -0.5, 0.0] };
+    let vertex4 = Vertex { position: [ -0.5, 0.5, 0.0] };
+    let shape = vec![vertex1, vertex2, vertex3, vertex4];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    // let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+    let indices_list:[u32; 6] = [0,1,3,1,2,3];
+    let indices = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList,
+                                      &indices_list).unwrap();
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+
     loop {
         let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 0.0, 1.0);
+        target.clear_color(0.1, 0.3, 0.3, 1.0);
+        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,
+            &Default::default()).unwrap();
         target.finish().unwrap();
 
         for ev in display.poll_events() {
